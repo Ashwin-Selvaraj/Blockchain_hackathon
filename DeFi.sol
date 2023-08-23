79727aaac03892a274dd1634457570989dafad24
Ashwin-Selvaraj <85512351+Ashwin-Selvaraj@users.noreply.github.com>
2023-08-23 21:28:08 +0530

    Update DeFi.sol

--- DeFi.sol
+++ DeFi.sol
@@ -83,6 +83,6 @@ contract DeFi {
         //uint256  interest =  (emi*12) - borrowedAmount[msg.sender];
         uint256 interest = (emi * 12) - borrowedAmount[msg.sender];
         return interest;   
-  //adding commands her
+  //adding commands here
   }
 }
